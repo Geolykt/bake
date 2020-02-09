3fa61d926f3a4a5d3c4a65a9ebf6906c22bc3800
Geolykt <admin@geolykt.de>
2020-02-09 21:27:37 +0100

    Hotfix for 1.5.0-pre
    Actually fixed the "%NEWLINE%"-bug.
    Seems like I did a rather foolish error that costed me a few hours.

--- bake/src/de/geolykt/bake/Bake.java
+++ bake/src/de/geolykt/bake/Bake.java
@@ -157,43 +157,19 @@ public class Bake extends JavaPlugin {
 				getLogger().info("Updating from the 1.4.1 config version (version 3) to the 1.5.0 config version (version 4). You may need to restart the server for it to take effect."); 
 				
 				String s = getConfig().getString("bake.chat.progress2", "");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().warning("Unsupported placeholder: the \"%NEWLINE%\"-placeholder no longer functions as of 1.5.0, use \"\\n\" instead. Updating has proven to be impossible automatically during tests. Will attempt to do it anyway");
-				}
-				s.replaceAll("%NEWLINE%", "\n");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().severe("Failed updating as expected. Please update the config file manually!");
-				}
+				s = s.replaceAll("%NEWLINE%", "\n");
 				getConfig().addDefault("bake.chat.progress2", s);
 				
 				s = getConfig().getString("bake.chat.contr2", "");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().warning("Unsupported placeholder: the \"%NEWLINE%\"-placeholder no longer functions as of 1.5.0, use \"\\n\" instead. Updating has proven to be impossible automatically during tests. Will attempt to do it anyway");
-				}
-				s.replaceAll("%NEWLINE%", "\n");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().severe("Failed updating as expected. Please update the config file manually!");
-				}
+				s = s.replaceAll("%NEWLINE%", "\n");
 				getConfig().addDefault("bake.chat.contr2", s);
 				
 				s = getConfig().getString("bake.chat.global.contr2", "");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().warning("Unsupported placeholder: the \"%NEWLINE%\"-placeholder no longer functions as of 1.5.0, use \"\\n\" instead. Updating has proven to be impossible automatically during tests. Will attempt to do it anyway");
-				}
-				s.replaceAll("%NEWLINE%", "\n");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().severe("Failed updating as expected. Please update the config file manually!");
-				}
+				s = s.replaceAll("%NEWLINE%", "\n");
 				getConfig().addDefault("bake.chat.global.contr2", s);
 				
 				s = getConfig().getString("bake.chat.finish2", "");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().warning("Unsupported placeholder: the \"%NEWLINE%\"-placeholder no longer functions as of 1.5.0, use \"\\n\" instead. Updating has proven to be impossible automatically during tests. Will attempt to do it anyway");
-				}
-				s.replaceAll("%NEWLINE%", "\n");
-				if (s.contains("%NEWLINE%")) {
-					getLogger().severe("Failed updating as expected. Please update the config file manually!");
-				}
+				s = s.replaceAll("%NEWLINE%", "\n");
 				getConfig().addDefault("bake.chat.finish2", s);
 				
 			}
